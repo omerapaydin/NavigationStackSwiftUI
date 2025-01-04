19441e8e77dd2137f5465e7debb5f00be7f2355f
Ömer Apaydın <omerapaydin90@gmail.com>
2025-01-04 15:54:24 +0300

    Initial Commit

---
+++ NavigationStackSwiftUI/NavigationStackSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackSwiftUIApp.swift
+//  NavigationStackSwiftUI
+//
+//  Created by Ömer on 4.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NavigationStackSwiftUITests/NavigationStackSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackSwiftUITests.swift
+//  NavigationStackSwiftUITests
+//
+//  Created by Ömer on 4.01.2025.
+//
+
+import Testing
+@testable import NavigationStackSwiftUI
+
+struct NavigationStackSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
